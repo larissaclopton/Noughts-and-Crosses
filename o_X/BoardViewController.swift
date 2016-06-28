46aa3a50426bf6e23a16850d6ac1220096c9b11e
larissaclopton <cloptla@gmail.com>
2016-06-28 20:07:33 +0200

    need to add all outlets

--- o_X/BoardViewController.swift
+++ o_X/BoardViewController.swift
@@ -6,10 +6,6 @@
 import UIKit
 
 class BoardViewController: UIViewController {
-
-    
-    @IBOutlet var BoardView: UIView!
-    @IBOutlet weak var NewGame: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,38 +13,7 @@ class BoardViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBAction func NewGameTapped(sender: UIButton) {
-        print("New game button pressed.")
-    }
     
-    @IBAction func CellOneTapped(sender: UIButton) {
-    }
-    
-    @IBAction func CellTwoTapped(sender: UIButton) {
-    }
-    
-    @IBAction func CellThreeTapped(sender: UIButton) {
-    }
 
-    @IBAction func CellFourTapped(sender: UIButton) {
-    }
-    
-    @IBAction func CellFiveTapped(sender: UIButton) {
-    }
-    
-    @IBAction func CellSixTapped(sender: UIButton) {
-    }
-    
-    @IBAction func CellSevenTapped(sender: UIButton) {
-    }
-    
-    @IBAction func CellEightTapped(sender: UIButton) {
-    }
-   
-    @IBAction func CellNineTapped(sender: UIButton) {
-    }
-    
-    
-    
 }
 
