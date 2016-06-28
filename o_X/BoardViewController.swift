a7fb957cb353684f0d948a42ff9240ab3c37ec9c
larissaclopton <cloptla@gmail.com>
2016-06-28 20:37:12 +0200

    New Game and cell IB actions/outlets added

--- o_X/BoardViewController.swift
+++ o_X/BoardViewController.swift
@@ -7,12 +7,21 @@ import UIKit
 
 class BoardViewController: UIViewController {
     
+    @IBOutlet weak var newGameButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    @IBAction func newGameButtonTapped(sender: UIButton) {
+        print("New Game button has been tapped.")
+    }
+    
+    @IBAction func cellTapped(sender: UIButton){
+            print("Cell \(sender.tag) has been tapped")
+    }
     
 
 }
