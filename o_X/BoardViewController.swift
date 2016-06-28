201518733a6100841745ba6ecda71177c7a0f314
larissaclopton <cloptla@gmail.com>
2016-06-28 19:15:33 +0200

    made IB actions for board elements

--- o_X/BoardViewController.swift
+++ o_X/BoardViewController.swift
@@ -7,13 +7,48 @@ import UIKit
 
 class BoardViewController: UIViewController {
 
+    
+    @IBOutlet var BoardView: UIView!
+    @IBOutlet weak var NewGame: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    // Create additional IBActions here.
+    @IBAction func NewGameTapped(sender: UIButton) {
+        print("New game button pressed.")
+    }
+    
+    @IBAction func CellOneTapped(sender: UIButton) {
+    }
+    
+    @IBAction func CellTwoTapped(sender: UIButton) {
+    }
+    
+    @IBAction func CellThreeTapped(sender: UIButton) {
+    }
 
+    @IBAction func CellFourTapped(sender: UIButton) {
+    }
+    
+    @IBAction func CellFiveTapped(sender: UIButton) {
+    }
+    
+    @IBAction func CellSixTapped(sender: UIButton) {
+    }
+    
+    @IBAction func CellSevenTapped(sender: UIButton) {
+    }
+    
+    @IBAction func CellEightTapped(sender: UIButton) {
+    }
+   
+    @IBAction func CellNineTapped(sender: UIButton) {
+    }
+    
+    
+    
 }
 
