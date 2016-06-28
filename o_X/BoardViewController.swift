dac93fdf06995d4c1c56b19caf9d3cc1eddadabf
larissaclopton <cloptla@gmail.com>
2016-06-28 17:44:41 +0200

    add basic UIelements

--- o_X/BoardViewController.swift
+++ o_X/BoardViewController.swift
@@ -7,17 +7,10 @@ import UIKit
 
 class BoardViewController: UIViewController {
 
-    @IBOutlet weak var newGameButton: UIButton!
-    // Create additional IBOutlets here.
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    @IBAction func newGameButtonPressed(sender: UIButton) {
-        print("New game button pressed.")
     }
     
     // Create additional IBActions here.
