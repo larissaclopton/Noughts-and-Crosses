8bcd324eac57c759257004c13a24e200a7f16800
larissaclopton <cloptla@gmail.com>
2016-06-30 20:58:18 +0200

    added register and login methods to User Controller

--- o_X/UserController.swift
+++ o_X/UserController.swift
@@ -14,16 +14,43 @@ class UserController {
     
     var currentUser = User()
     
+    var registeredUsers: [User] = []
+    
+    public var currentlyLoggedIn: User?
+    
     func register(email email: String, password: String, onCompletion: (User?, String?) -> Void) {
+        
+        var userExists = false
+        
+        for index in 0 ..< registeredUsers.count {
+            if (email == registeredUsers[index].email) {
+                userExists = true
+                break
+            }
+        }
+        
+        if (userExists) {
+            onCompletion(nil, "Email address already exists.")
+        }
+        else if password.characters.count < 6 {
+            onCompletion(nil, "Password must be at least 6 characters.")
+        }
+        else {
+            currentUser.email = email
+            currentUser.password = password
+            onCompletion(currentUser, nil)
+            currentlyLoggedIn = currentUser
+        }
         
     }
     
     func login(email email: String, password: String, onCompletion: (User?, String?) -> Void) {
-        
+        currentlyLoggedIn = currentUser
     }
     
     func logout(onCompletion onCompletion: (String?) -> Void) {
-        
+        currentlyLoggedIn = nil
     }
 
 }
+
