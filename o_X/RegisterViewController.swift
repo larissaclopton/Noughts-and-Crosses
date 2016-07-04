0649802efec9a7f232744e3794859e65e99051f5
larissaclopton <cloptla@gmail.com>
2016-07-04 18:15:21 +0200

    6B finished

--- o_X/RegisterViewController.swift
+++ o_X/RegisterViewController.swift
@@ -19,11 +19,6 @@ class RegisterViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     @IBAction func registerButtonTapped(sender: UIButton) {
         
